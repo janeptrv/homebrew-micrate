f5e19569a9e64116c75922ae972bb4b3c2b1f32f
Juan Edi <jedi11235@gmail.com>
2016-07-01 11:20:12 -0300

    Bump to version 0.2.1

--- micrate.rb
+++ micrate.rb
@@ -5,8 +5,8 @@
 class Micrate < Formula
   desc "Database migration tool written in Crystal"
   homepage "http://github.com/juanedi/micrate"
-  url "https://github.com/juanedi/micrate/releases/download/v0.2.0/micrate-0.2.0-darwin-x86_64.tar.gz"
-  sha256 "015ef4b077f1412c946c689e64b8f919bed8fb0aed5b3d6cf3f136b32dfad126"
+  url "https://github.com/juanedi/micrate/releases/download/v0.2.1/micrate-0.2.1-darwin-x86_64.tar.gz"
+  sha256 "35a470a1b874b9822c574cc5e986a10c70cdb97e0607b83b5b0b9dd349de335e"
 
   def install
     bin.install "micrate"
