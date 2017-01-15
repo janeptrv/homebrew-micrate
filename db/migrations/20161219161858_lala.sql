759b0ab9ecaf73e45297e0648c4a0df7cc520a04
Juan Edi <jedi@manas.com.ar>
2017-01-15 15:40:06 -0300

    Delete accidentally pushed test files

--- db/migrations/20161219161858_lala.sql
+++
@@ -1,9 +0,0 @@
--- +micrate Up
--- SQL in section 'Up' is executed when this migration is applied
-CREATE TABLE users(id INT PRIMARY KEY, email VARCHAR NOT NULL);
-
-
-
--- +micrate Down
--- SQL section 'Down' is executed when this migration is rolled back
-DROP TABLE users;
